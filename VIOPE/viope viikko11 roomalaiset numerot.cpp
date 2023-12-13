155f2595ab419438db514b2cfd079b195c570c28
SpaceRodento <spacerodento@gmail.com>
2023-12-13 10:11:22 +0200

    Kaikki puuhattu

--- VIOPE/viope viikko11 roomalaiset numerot.cpp
+++ VIOPE/viope viikko11 roomalaiset numerot.cpp
@@ -1,108 +1,97 @@
-//Kirjastot
-#include <iostream>
-#include <string>
-#include <cmath>
-#include <vector>
-  using namespace std;
-//======================
-/* 
-Tee ohjelma, joka muuttaa roomalaiset numerot kymmenjärjestelmän luvuiksi. 
-Muunnos voidaan ajatella tehtäväksi aliohjelmassa, jonne viedään parametrinä luvun string tyyppinen muuttuja.
-Paluuarvona saatu luku voidaan tulostaa pääohjelmassa.
-Example output:
-Roomalainen luku, jonka desimaaliarvon haluat tietää? MCMXCVI
-Desimaalimuodossa roomalainen luku MCMXCVI on 1996
+  //Kirjastot
+  #include <iostream>
+  #include <string>
+  #include <cmath>
+  #include <vector>
+    using namespace std;
+  //======================
+  /* 
+  Tee ohjelma, joka muuttaa roomalaiset numerot kymmenjärjestelmän luvuiksi. 
+  Muunnos voidaan ajatella tehtäväksi aliohjelmassa, jonne viedään parametrinä luvun string tyyppinen muuttuja.
+  Paluuarvona saatu luku voidaan tulostaa pääohjelmassa.
+  Example output:
+  Roomalainen luku, jonka desimaaliarvon haluat tietää? MCMXCVI
+  Desimaalimuodossa roomalainen luku MCMXCVI on 1996
 
-Roomalainen numero
-I 1
-V 5
-X 10
-L 50
-C 100
-D 500
-M 1000​
-*/
+  Roomalainen numero
+  I 1
+  V 5
+  X 10
+  L 50
+  C 100
+  D 500
+  M 1000​
+  */
 
-// Struct
-struct structRoomalaisNumero{ // Strukti johon syötetään yksittäinen merkki
-  char merkki;
-  int arvo;
-}; 
+  // Struct
+  struct structRoomalaisNumero{ // Strukti johon syötetään yksittäinen merkki
+    char merkki;
+    int arvo;
+  }; 
+  //======== Aliohjelma: Muunna roomalaisia numeroita arabiaksi
+  long LaskeRoomalaiset(const string& Roomalainen){
+    long lopputulos = 0;
+      // otetaan stringinä vastaan asioita ja annetaan vastaava numero
+    vector<structRoomalaisNumero> roomalaisNumeroTaulu = { // luodaan muunnostaulu kirjaimista numeroihin
+      {'I', 1},
+      {'V', 5},
+      {'X', 10},
+      {'L', 50},
+      {'C', 100},
+      {'D', 500},
+      {'M', 1000},
+    };
+      long Arabialainen = 0;
 
-//======== Aliohjelma: Muunna roomalaisia numeroita arabiaksi
-long LaskeRoomalaiset(const string& Roomalainen){
-    // otetaan stringinä vastaan asioita ja väännetään niistä numeroita
-  vector<structRoomalaisNumero> roomalaisNumeroTaulu = { // luodaan muunnostaulu kirjaimista numeroihin
-    {'I', 1},
-    {'V', 5},
-    {'X', 10},
-    {'L', 50},
-    {'C', 100},
-    {'D', 500},
-    {'M', 1000},
-  };
-    long Arabialainen = 0;
-
-// Luodaan vektori käännetyistä numeroista
-vector<int> vecArabialaisLuvut;
-//Käydään läpi forrilla roomalaiset ja käännetään niitä yksittäin
-for (char c : Roomalainen){
-  for (structRoomalaisNumero& pari : roomalaisNumeroTaulu){ //määritellään arabian pari roomalaisnumerolle
-    if (pari.merkki == c){
-      Arabialainen = pari.arvo;
-      vecArabialaisLuvut.push_back(Arabialainen); //pusketaan vektorin perälle löydetty numero
+  vector<int> vecArabialaisLuvut;   // Luodaan vektori käännetyistä numeroista
+    for (char c : Roomalainen){       //Käydään läpi forrilla roomalaiset ja käännetään niitä yksittäin
+      for (structRoomalaisNumero& pari : roomalaisNumeroTaulu){ //määritellään arabian pari roomalaisnumerolle
+        if (pari.merkki == c){
+          Arabialainen = pari.arvo;
+          vecArabialaisLuvut.push_back(Arabialainen); //pusketaan vektorin perälle löydetty numero
+        }
+      }
     }
-  }
-}
-for (int luku : vecArabialaisLuvut){
-  cout << luku << " ";
-}
 
-/*
-// forri läpikäyntiin
-for (char c : Roomalainen ) // Range based for loop. (Eli tämän: 0; i < count; i++) i:n sijaan käydäänkin kirjaimet läpi)
-{
-  // käydään vektori läpi ja todetaan koska merkki vastaa taulun tietoa ja sylkästään se pihalle, sit palataan edelliseen forriin
-  for (structRoomalaisNumero& pari : roomalaisNumeroTaulu){ // luodaan structRoomalaisnumeroon "pari"-olento ja sen ominaisuutena on roomalaisnumero
-    if (pari.merkki == c){  // Tarkastellaan kohtaa c ja mikäli taulusta löytyy annettu merkki...
-      Arabialainen += pari.arvo;  // ...ynnätään löydetty numero arabialaisittain
+// ==== LOPPUTULOSLASKELMA ====
+// Käydään tämä luotu arabialaisvektori läpi ja verrataan niitä keskenään, ynnätään tai vähennellään loppusummaksi
+    int edellinen = vecArabialaisLuvut[0];  // alustetaan edelliseksi vektorin ensimmäinen luku
+      for_each(vecArabialaisLuvut.begin(), vecArabialaisLuvut.end(), [&](int nykyinen){ // käydään for_eachillä luvut läpi vektorissa
+        if (nykyinen <= edellinen){     // 1<5 VI
+            lopputulos += nykyinen;
+              // cout << " (nykyinen+): " << nykyinen ;          
+              // cout << " (ed+): "      << edellinen ;          
+              // cout << " lopputulos: " << lopputulos << endl;
+        }
 
-// Muutenkiva mutta nyt numerot vain ynnäytyy. tarvii keksiä logiikka niiden vähentelyyn
-// Kokeillaas että sikälimikäli vasemmalla on isompi kuin oikealla, vähennä. muuten ynnää
+          else if (nykyinen > edellinen){ // 5>1 IV
+            lopputulos = lopputulos+nykyinen - 2*edellinen; // näyttää oudolta, mutta vähentelystä tuli muuten 0
+              // cout << " (nykyinen-): " << nykyinen ;          
+              // cout << " (ed-): "      << edellinen ;          
+              // cout << " lopputulos: " << lopputulos << endl;
+          }
+        edellinen = nykyinen;
+      });
 
-// IX = 9, XI = 11
-//
-
-
-
-
-      break;
-    }
-  }
-}
-return Arabialainen;
-}
+/*      // testinä tulostellaan vektorin kaikki lukemat ulos
+      for (int luku : vecArabialaisLuvut){
+      cout << "vektori: "<< luku << " " << endl;}
 */
+      return lopputulos;
+  }
 
+  // ========== PÄÄOHJELMA ====================
+  int main(){
+    string Roomalainen;
 
+  cout << "Roomalainen luku, jonka desimaaliarvon haluat tietää?";
+  cin >> Roomalainen;
+  cout << endl;
 
+    long Arabialaisnumero =   LaskeRoomalaiset(Roomalainen);
+    cout << "Desimaalimuodossa roomalainen luku " << Roomalainen <<" on " << Arabialaisnumero << endl;
 
-// ========== PÄÄOHJELMA ====================
-int main() {
-//  const int size = 20;
-//  int i = 0;
-  string Roomalainen;
-
-cout << "Roomalainen luku, jonka desimaaliarvon haluat tietää?";
-cin >> Roomalainen;
-cout << endl;
-
-  long Arabialaisnumero =   LaskeRoomalaiset(Roomalainen);
-  cout << "Desimaalimuodossa roomalainen luku " << Roomalainen <<" on " << Arabialaisnumero << endl;
-
-//cout << Arabialaisnumero << endl;
-
-return 0;
-}
+  return 0;
+  }
 
 
